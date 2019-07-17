eba60af6eece1a75c32162b1fc35cc111f2efbe9
Rachell Sincere <rachellsincere@gmail.com>
2019-07-16 21:16:45 -0400

    set up componentDidMount, deleted app.test, started building Friends.js component

---
+++ friends/src/Friends.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Friends extends Component {
+  render() {
+    return (
+      <div className="friends">
+      
+      </div>
+    );
+  }
+}
+
+export default Friends;
