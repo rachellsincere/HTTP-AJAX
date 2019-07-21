e88cd71e7880bdc0d885838272400fdc16551fb0
Rachell Sincere <rachellsincere@gmail.com>
2019-07-21 17:22:47 -0400

    moved data fetch from friends component to app component

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,22 +3,29 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  state = {
+  // add constructor and CDM
+  constructor() {
+    super();
+  this.state = {
     friends: []
-  }
-  componentDidMount() {
-    axios
-      .get("http://localhost:5000/friends")
-      .then(response => {this.setState({friends: response.data})})
-      .catch(err => console.log("Error", err))
-  }
-  render() {
-    return (
-      <div className="App">
-        {/* <Friends /> */}
-      </div>
-    );
-  }
+  };
+}
+
+componentDidMount() { 
+  //fetch initial data needed to set the state
+  axios
+    .get("http://localhost:5000/friends")
+    .then(response => {this.setState({friends: response.data})})
+    .catch(err => console.log("Error", err))
+}
+
+render() {
+  return (
+    <div className="App">
+      {/* <Friends /> */}
+    </div>
+  );
+}
 }
 
 export default App;
--- friends/src/Friends.js
+++ friends/src/Friends.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+
 
 class Friends extends Component {
-  render() {
-    return (
-      <div className="friends">
-      
-      </div>
-    );
-  }
+    
 }
 
 export default Friends;
